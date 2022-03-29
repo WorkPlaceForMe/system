f6a73fa2f3b402bedc2583dee601e21234201210
ridweng <i93kaiser@hotmail.com>
2022-03-29 18:15:32 +0100

    Logs for the process

--- src/controllers/serial.controllers.js
+++ src/controllers/serial.controllers.js
@@ -7,7 +7,6 @@ exports.check = async (req, res) => {
     const data = req.body
     const bytes  = crypt.AES.decrypt(data.snd, process.env.SECRETLOCAL)
     let comparison = bytes.toString(crypt.enc.Utf8).split('/|')
-
     Serial.findOne({
         where: {
           serial: comparison[0]
@@ -15,7 +14,10 @@ exports.check = async (req, res) => {
       })
         .then(serial => {
             if (!serial) {
+                console.log("Writting serial key...")
                 let ser = comparison.shift();
+                const hrdw = comparison.join('/|')
+                const encrypted = crypt.AES.encrypt(hrdw, process.env.SECRETSERVER).toString()
                 return Serial.create({
                     serial: ser,
                     uuidOS: comparison[0],
@@ -26,22 +28,33 @@ exports.check = async (req, res) => {
                     systemModel: comparison[5],
                     version: comparison[6],
                     diskSerial: comparison[7],
-                    hardware: comparison.join('/|')
+                    hardware: encrypted
                   })
                     .then(_serial => {
+                        result = "Successfully wrote: Serial saved."
+                        console.log(result)
                         let value = crypt.AES.encrypt('true', process.env.SECRETLOCAL).toString()
                         res.status(201).send({ success: true, message: 'Serial has been registered successfully.', auth: value })
                     })
                     .catch(err => {
+                        result = "Unsuccessfully wrote: Database failled saved."
+                        console.log(result)
                         let value = crypt.AES.encrypt('false', process.env.SECRETLOCAL).toString()
                         res.status(500).send({ success: false, message: err.message,  auth: value})
                     })
             }else{
+                console.log('Reading serial key...')
                 comparison.shift();
-                if(comparison.join('/|') === serial.dataValues.hardware){
+                const bytes  = crypt.AES.decrypt(serial.dataValues.hardware, process.env.SECRETSERVER);
+                const comp = bytes.toString(crypt.enc.Utf8);
+                if(comparison.join('/|') === comp){
+                    result = "Successfully read: Serial matched."
+                    console.log(result)
                     let value = crypt.AES.encrypt('true', process.env.SECRETLOCAL).toString()
                     res.status(202).send({ success: true, auth: value })
                 }else{
+                    result = "Unsuccessful read: Serial don't match with the present one."
+                    console.log(result)
                     let value = crypt.AES.encrypt('false', process.env.SECRETLOCAL).toString()
                     res.status(401).send({ success: true, auth: value })
                 }
