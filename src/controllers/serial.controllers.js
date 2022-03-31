1e442e503141c38e37e6c83552be45813d8aab0f
ridweng <i93kaiser@hotmail.com>
2022-03-31 17:25:53 +0100

    Serial handler when missing

--- src/controllers/serial.controllers.js
+++ src/controllers/serial.controllers.js
@@ -15,34 +15,10 @@ exports.check = async (req, res) => {
       })
         .then(serial => {
             if (!serial) {
-                // console.log("Writting serial key...")
-                // let ser = comparison.shift();
-                // const hrdw = comparison.join('/|')
-                // const encrypted = crypt.AES.encrypt(hrdw, process.env.SECRETSERVER).toString()
-                // return Serial.create({
-                //     serial: ser,
-                //     uuidOS: comparison[0],
-                //     uuidHardware: comparison[1],
-                //     osHostname: comparison[2],
-                //     diskSize: comparison[3],
-                //     kernel: comparison[4],
-                //     systemModel: comparison[5],
-                //     version: comparison[6],
-                //     diskSerial: comparison[7],
-                //     hardware: encrypted
-                //   })
-                //     .then(_serial => {
-                //         result = "Successfully wrote: Serial saved."
-                //         console.log(result)
-                //         let value = crypt.AES.encrypt('true', process.env.SECRETLOCAL).toString()
-                //         res.status(201).send({ success: true, message: 'Serial has been registered successfully.', auth: value })
-                //     })
-                //     .catch(err => {
-                //         result = "Unsuccessfully wrote: Database failled saving."
-                //         console.log(result)
-                //         let value = crypt.AES.encrypt('false', process.env.SECRETLOCAL).toString()
-                //         res.status(500).send({ success: false, message: err.message,  auth: value})
-                //     })
+                result = "Unsuccessfully read: No serial present."
+                console.log(result)
+                let value = crypt.AES.encrypt('false', process.env.SECRETLOCAL).toString()
+                res.status(401).send({ success: false, message: result,  auth: value})
             }else if (serial && serial.dataValues.hardware === null){
                 console.log("Writting serial key...")
                 let ser = comparison.shift();
